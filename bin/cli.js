fcb30756746e7cba1876d6c17bfd24788f20563c
wusiyi <wusiyi@wusiyideMacBook-Pro.local>
2021-04-16 17:42:39 +0800

    feat(/): add #!/usr/bin/env node

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,13 @@
+#!/usr/bin/env node
+/*
+ * @Author: your name
+ * @Date: 2021-04-16 10:46:42
+ * @LastEditTime: 2021-04-16 17:41:09
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /hacker-cli/bin/cli.js
+ */
 'use strict';
-
 const Command = require('../lib/command.js');
 
 
---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Shawn Wu
+ * @Date: 2021-04-16 10:48:21
+ * @LastEditTime: 2021-04-16 17:37:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /hacker-cli/index.js
+ */
+exports.Action = require('./lib/action');
+exports.Command = require('./lib/command');
+exports.Download = require('./lib/download');
+// exports.WebpackTool = require('webpack-tool');
+// exports.utils = require('./lib/utils');
+// exports.webpack = exports.WebpackTool.webpack;
+// exports.merge = exports.WebpackTool.merge;
